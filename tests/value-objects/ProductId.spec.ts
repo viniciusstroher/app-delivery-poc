22bc0f4124249c1f9014a51e75718e72958d49fd
veni <viniciusstroher@gmail.com>
2020-12-20 21:42:02 -0300

    ajuste test de productId

--- tests/value-objects/ProductId.spec.ts
+++ tests/value-objects/ProductId.spec.ts
@@ -2,11 +2,15 @@ import { UuidGenerator } from "@infra/utils/UuidGenerator"
 import { ProductId } from "@domain/value-objects/ProductId"
 
 describe('Testing ProductId Vo Class', () => {
-    test('Uuid return true', () => {
+    test('should return Uuid true', () => {
         const uuidGenerator = new UuidGenerator();
         const uuid:string = uuidGenerator.generate();
         
         const productIdVo:ProductId = ProductId.create(uuid);
         expect(productIdVo.getId()).toBe(uuid);
+    });
+
+    test('should throw exception if productid is null', () => {
+        expect(() => ProductId.create("")).toThrow();
     });
 });
